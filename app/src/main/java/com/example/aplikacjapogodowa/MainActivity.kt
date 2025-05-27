1dd88dbdab8295010b41456792bd8d7d47f66c41
krzpu <krzpu@int.pl>
2025-05-27 22:31:56 +0200

    dodano nowy ekran i obsługę przycisków do przejścia na kolejny ekran

--- app/src/main/java/com/example/aplikacjapogodowa/MainActivity.kt
+++ app/src/main/java/com/example/aplikacjapogodowa/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.aplikacjapogodowa
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : ComponentActivity() {
+
+    // Deklaracja pól dla widoków
+    private lateinit var searchEditText: EditText
+    private lateinit var searchButton: Button
+    private lateinit var gpsButton: Button
+    private lateinit var listaUlubionych: ListView
+
+    // główna funkcja onCreate, która jest wywoływana przy tworzeniu aktywności
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        searchEditText    = findViewById(R.id.searchEditText)
+        searchButton      = findViewById(R.id.searchButton)
+        gpsButton         = findViewById(R.id.gpsButton)
+        listaUlubionych   = findViewById(R.id.ulubioneListView)
+
+
+        searchButton.setOnClickListener {
+            val miasto = searchEditText.text.toString().trim()
+            if (miasto.isNotEmpty()) {
+                val intent = Intent(this, szczegoly::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Wpisz nazwę miasta", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        gpsButton.setOnClickListener {
+            val intent = Intent(this, szczegoly::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
---
+++ app/src/main/java/com/example/aplikacjapogodowa/szczegoly.kt
@@ -0,0 +1,17 @@
+package com.example.aplikacjapogodowa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class szczegoly : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_szczegoly)
+
+    }
+}
